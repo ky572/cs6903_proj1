04584f9387282b172b924e715632cd13b977fa79
ctkhanhly <ctkhanhly@gmail.com>
2021-03-10 15:54:43 -0500

    added unit tests

---
+++ 4_5_test.py
@@ -0,0 +1,423 @@
+from 4_5 import Decrypter
+from encryption_test import Cipher_Generator 
+from collections import defaultdict
+import random
+
+def Dectypter_Tester:
+
+    def __init__(self):
+        self.cipher_generator = Cipher_Generator()
+        self.decrypter = Decrypter()
+        random.seed()
+        
+
+    def run_tests(self):
+        
+        #ciphertext test:
+        self.cipher_generator_test()
+
+        #test decrypter's global variables
+        print("plain_text1", plain_text1)
+        print("plain_text2", plain_text2)
+        print("plain_text3", plain_text3)
+        print("plain_text4", plain_text4)
+        print("plain_text5", plain_text5)
+
+
+        print("plain_text_1", plain_text_1)
+        print("plain_text_2", plain_text_2)
+        print("plain_text_3", plain_text_3)
+        print("plain_text_4", plain_text_4)
+        print("plain_text_5", plain_text_5)
+
+        print("alphabet", alphabet)
+        print("words", words)
+
+        #test for 1
+        self.convert_string_to_vec_chars_tester("hello world")
+
+        #test for 2
+        self.find_distribution_n_grams_tester()
+
+        #test for 3
+        self.get_terms_ordered_by_dec_freq_tester()
+
+        #test for 4
+        self.get_grouped_n_grams_ordered_by_dec_freq_tester()
+
+        #test for 5
+        self.get_n_gram_converter_tester()
+
+        #test for 6
+        self.map_two_distributions_tester()
+
+        #test for 7
+        self.concate_2_defaultdict_tester()
+
+        #test for 8
+        self.convert_list_chars_to_string_tester()
+
+        #test for 9
+        self.change_char_at_tester()
+
+        #test for 10
+        self.try_1_edit_distance_away_groups_tester()
+
+        #test for 11
+        self.try_decode_2_gram_tester()
+
+        #test for 12
+        self.try_decode_1_gram_tester()
+
+        #test for 13
+        self.get_grouped_n_gram_converter_tester()
+
+        #test for 14
+        self.decrypt_key_tester()
+
+        #test for 15
+        self.calc_edit_distance_tester()
+
+        #test for 16
+        self.get_most_similar_plaintext_tester()
+
+        #test for 17
+        self.decrypt_type_two_tester()
+
+        #test for 18
+        self.get_most_likely_word_tester()
+
+        #test for 19
+        self.correct_type_two_decrytion_tester()
+
+
+
+    def cipher_generator_test(self):
+        print()
+        print("===========================================================================")
+        print("================ TEST FOR Cipher_Generator =====================")
+        self.cipher_generator.generate_test1_cipher()
+        self.cipher_generator.generate_test1_cipher(1, True, 20)
+        print("===========================================================================")
+        print()
+
+    #test for 1
+    def convert_string_to_vec_chars_tester(self, string):
+        converted_string = self.decrypter.convert_string_to_vec_chars(string)
+        print()
+        print("===========================================================================")
+        print("================ TEST FOR convert_string_to_vec_chars =====================")
+        print('Input: %s\n. Output: %s', string, converted_string)
+        print("===========================================================================")
+        print()
+
+    #test for 2
+    def find_distribution_n_grams_tester(self):
+        
+        print()
+        print("===========================================================================")
+        print("================ TEST FOR find_distribution_n_grams =====================")
+        for n_gram in range(3):
+            dist = self.decrypter.find_distribution_n_grams(plain_text1, n_gram)
+            print(f'plaintext1 n_gram={n_gram} distribution')
+            for key, val in dist.items():
+                print(f'n_gram: {key}, frequency: {val}')
+        print("===========================================================================")
+        print()
+
+    #test for 3
+    def get_terms_ordered_by_dec_freq_tester(self):
+        print()
+        print("===========================================================================")
+        print("================ TEST FOR get_terms_ordered_by_dec_freq =====================")
+
+        dist = defaultdict(lambda : 0)
+        for char in alphabet:
+            freq = random.randint(50)
+            dist[char] = freq
+            print(f'char: {char}, freq: {freq}')
+
+        n_gram_list = self.decrypter.get_terms_ordered_by_dec_freq(dist)
+        print("n_gram_list", n_gram_list)
+
+        print("===========================================================================")
+        print()
+
+    #test for 4
+    def get_grouped_n_grams_ordered_by_dec_freq_tester(self):
+
+        print()
+        print("===========================================================================")
+        print("================ TEST FOR get_grouped_n_grams_ordered_by_dec_freq =====================")
+        dist = defaultdict(lambda : 0)
+        n_gram = 2
+        print('n_gram is', n_gram)
+        for i in range(len(alphabet) - n_gram + 1 ):
+            chars = alphabet[i : i+n_gram ]
+            freq = random.randint(50)
+            dist[chars] = freq
+            print(f'chars: {chars}, freq: {freq}')
+        
+        grouped_n_gram_list = self.decrypter.get_grouped_n_grams_ordered_by_dec_freq(dist)
+        print("n_gram_list", n_gram_list)
+        print("===========================================================================")
+        print()
+
+    #test for 5
+    def get_n_gram_converter_tester(self):
+        print()
+        print("===========================================================================")
+        print("================ TEST FOR get_n_gram_converter =====================")
+        ordered_n_gram_cipher = ["abc", "geq", "adf", "qer", "fqe"]
+        ordered_n_gram_plain = ["tre", "fbq", "fqe", "eqw"]
+        for i in range(3):
+
+            # cipher list has 1 more term for test 1
+            if i == 1: #equal size for test 2
+                ordered_n_gram_plain.append("utw")
+            elif i == 2:# plain list has 1 more term for test 3
+                ordered_n_gram_plain.append("xyz")
+
+            print(f"====TEST {i+1}===")
+            print("ordered_n_gram_cipher", ordered_n_gram_cipher)
+            print("ordered_n_gram_plain", ordered_n_gram_plain)
+            n_gram_converter = self.decrypter.get_n_gram_converter(ordered_n_gram_cipher, ordered_n_gram_plain)
+            print('n_gram_converter is' )
+            for n_gram_cipher, n_gram_plain in n_gram_converter.items():
+                print(f'n_gram_cipher: {n_gram_cipher}, n_gram_plain: {n_gram_plain}')
+
+        print("===========================================================================")
+        print()
+
+    #test for 6
+    def map_two_distributions_tester(self)
+
+        print()
+        print("===========================================================================")
+        print("================ TEST FOR map_two_distributions =====================")
+
+        ordered_n_gram_cipher = ["abc", "geq", "adf", "qer", "fqe"]
+        ordered_n_gram_plain = ["tre", "fbq", "fqe", "eqw", "xyz"]
+
+        n_gram_converter = defaultdict(str, zip(ordered_n_gram_cipher,ordered_n_gram_plain))
+        decrypted_ciphertext = list("abcadfgeqqerfqegeqabcadfabcqeradfgeqabcabcgeqabc")
+        print("decrypted_ciphertext before", decrypted_ciphertext)
+        start = 0
+        t = 5
+        self.decrypter.map_two_distributions(n_gram_converter, decrypted_ciphertext, start, t)
+        print("decrypted_ciphertext after", decrypted_ciphertext)
+
+        print("===========================================================================")
+        print()
+
+    #test for 7
+    def concate_2_defaultdict_tester(self):
+
+        print()
+        print("===========================================================================")
+        print("================ TEST FOR concate_2_defaultdict =====================")
+
+        ordered_n_gram_cipher = ["abc", "geq"]
+        ordered_n_gram_plain = ["tre", "fbq"]
+        org_dict = defaultdict(str, zip(ordered_n_gram_cipher,ordered_n_gram_plain))
+
+        ordered_n_gram_cipher = ["adf", "qer", "fqe"]
+        ordered_n_gram_plain = ["fqe", "eqw", "xyz"]
+        add_dict = defaultdict(str, zip(ordered_n_gram_cipher,ordered_n_gram_plain))
+        print("========= BEFORE =============")
+        print('org_dict')
+        for key, val in org_dict.items():
+            print(f'{key}: {val}')
+        print('add_dict')
+        for key, val in add_dict.items():
+            print(f'{key}: {val}')
+        self.decrypter.concate_2_defaultdict(org_dict, add_dict)
+        print("========= AFTER =============")
+        print('org_dict')
+        for key, val in org_dict.items():
+            print(f'{key}: {val}')
+        print("===========================================================================")
+        print()
+
+    #test for 8
+    def convert_list_chars_to_string_tester(lst):
+        print()
+        print("===========================================================================")
+        print("================ TEST FOR convert_list_chars_to_string =====================")
+
+        lst = ['a', 'b', 'c', 'd', 'e', 'f']
+        string = self.decrypter.convert_list_chars_to_string(lst)
+        print(f'lst is: {lst}')
+        print(f'string is: {string}')
+        print("===========================================================================")
+        print()
+
+
+    #test for 9
+    def change_char_at_tester(self):
+
+        print()
+        print("===========================================================================")
+        print("================ TEST FOR change_char_at =====================")
+        string = "abcdef"
+        index = 2
+        char = 'Z'
+        print(f"string before: {string}")
+        string = self.decrypter.change_char_at(string, index, char)
+        print(f"string after: {string}")
+        print("===========================================================================")
+        print()
+
+
+    #test for 10
+    #TO BE FINISHED
+    def try_1_edit_distance_away_groups_tester(self):
+        print()
+        print("===========================================================================")
+        print("================ TEST FOR try_1_edit_distance_away_groups =====================")
+        shared_n_gram_group = None
+        grouped_n_gram_freq_cipher = None
+        grouped_n_gram_freq_plain = None
+        n_gram_converter = None
+        self.decrypter.try_1_edit_distance_away_groups(shared_n_gram_group, grouped_n_gram_freq_cipher, grouped_n_gram_freq_plain, n_gram_converter)
+        print("===========================================================================")
+        print()
+
+    #test for 11
+    def try_decode_2_gram_tester(self):
+        print()
+        print("===========================================================================")
+        print("================ TEST FOR try_decode_2_gram =====================")
+
+        shared_n_gram_group = None
+        grouped_n_gram_freq_cipher = None
+        grouped_2_gram_freq_plain = None
+        n_gram_converter = None
+
+        self.decrypter.try_decode_2_gram(shared_n_gram_group, grouped_n_gram_freq_cipher, grouped_2_gram_freq_plain, n_gram_converter)
+        print("===========================================================================")
+        print()
+
+
+    #test for 12
+    #TO BE FINISHED
+    def try_decode_1_gram_tester(self):
+        print()
+        print("===========================================================================")
+        print("================ TEST FOR try_decode_1_gram =====================")
+
+        shared_n_gram_group = None
+        grouped_n_gram_freq_cipher = None
+        ordered_1_gram_plain = None
+        n_gram_converter = None
+
+        self.decrypter.try_decode_1_gram(shared_n_gram_group, grouped_n_gram_freq_cipher, ordered_1_gram_plain, n_gram_converter)
+        print("===========================================================================")
+        print()
+
+
+    #test for 13
+    #TO BE FINISHED
+    def get_grouped_n_gram_converter_tester(self):
+        print()
+        print("===========================================================================")
+        print("================ TEST FOR get_grouped_n_gram_converter =====================")
+
+        grouped_n_gram_freq_cipher = None
+        grouped_n_gram_freq_plain = None
+        grouped_2_gram_freq_plain = None
+        ordered_1_gram_plain = None
+
+        self.decrypter.get_grouped_n_gram_converter(grouped_n_gram_freq_cipher, grouped_n_gram_freq_plain, grouped_2_gram_freq_plain, ordered_1_gram_plain)
+        print("===========================================================================")
+        print()
+
+
+    #test for 14
+    #TO BE FINISHED
+    def decrypt_key_tester(self):
+        print()
+        print("===========================================================================")
+        print("================ TEST FOR decrypt_key =====================")
+
+        t = None
+        ciphertext = None
+        n_gram = None
+
+        self.decrypter.decrypt_key(t, ciphertext, ordered_1_gram_plain, n_gram)
+        print("===========================================================================")
+        print()
+
+    #test for 15
+    def calc_edit_distance_tester(self):
+        print()
+        print("===========================================================================")
+        print("================ TEST FOR calc_edit_distance =====================")
+
+        for decrypted_ciphertext,plaintext in zip(["abcdef", "abcd", "abcfef"], ["abcfef", "abcfef", "abc"]) 
+            
+            distance = self.decrypter.calc_edit_distance(decrypted_ciphertext, plaintext)
+            print(f"decrypted_ciphertext: {decrypted_ciphertext}, plaintext: {plaintext}. Edit distance: {distance}")
+
+        print("===========================================================================")
+        print()
+
+    #test for 16
+    def get_most_similar_plaintext_tester(self):
+        print()
+        print("===========================================================================")
+        print("================ TEST FOR get_most_similar_plaintext =====================")
+        decrypted_ciphertexts = ["hqllo wozld abcxyz etd henlo wcrld", "hqllo wozld abcxyz etd henlo wcrld", "hqllo wozld abcxyz etd henlo wcrld", "hqllo wozld abcxyz etd henlo wcrld"]
+        plaintexts = [ "hello world abcxyz end hello world", "hafo world mnopze end hello world", "hafo world abcdef end hello world", "hafo world abcxyz end hello world"]
+        print('decrypted_ciphertexts')
+        for decrypted_ciphertext in decrypted_ciphertexts:
+            print(decrypted_ciphertext)
+        print('plaintexts')
+        for plaintext in plaintexts:
+            print(plaintext)
+        plaintext = self.decrypter.get_most_similar_plaintext(decrypted_ciphertexts, plaintexts)
+        print("=========== CHOSEN PLAINTEXT ==========")
+        print(plaintext)
+        print("===========================================================================")
+        print()
+
+    #test for 17
+    #TO BE FINISHED
+    def decrypt_type_two_tester(self):
+
+        print()
+        print("===========================================================================")
+        print("================ TEST FOR decrypt_type_two =====================")
+        ciphertext = None
+        n_gram = None
+        self.decrypter.decrypt_type_two(ciphertext, n_gram)
+        print("===========================================================================")
+        print()
+
+
+    #test for 18
+    def get_most_likely_word_tester(self):
+        print()
+        print("===========================================================================")
+        print("================ TEST FOR get_most_similar_plaintext =====================")
+
+        decrypted_text = "azeswmeness"
+        start_index = 0
+        chose_word = self.decrypter.get_most_likely_word(decrypted_text, start_index)
+        print(f"decrypted_text: {decrypted_text}, start_index: {start_index}, chose_word: {chose_word}")
+        print("===========================================================================")
+        print()
+
+    #test for 19
+    def correct_type_two_decrytion_tester(self):
+
+        print()
+        print("===========================================================================")
+        print("================ TEST FOR correct_type_two_decrytion =====================")
+        decrypted_text = "azeswmeness atzentiunal"
+        corrected_decryption = self.decrypter.correct_type_two_decrytion(decrypted_text)
+        print(f"decrypted_text: {decrypted_text}, corrected_decryption: {corrected_decryption}")
+
+        print("===========================================================================")
+        print()
+
--- encryption_test.py
+++ encryption_test.py
@@ -1,47 +1,84 @@
 import random
 
-possible_chars='abcdefghijklmnopqrstuvwxyz '
 
-test1_plaintexts = [
-    'cabooses meltdowns bigmouth makework flippest neutralizers gipped mule antithetical imperials carom masochism stair retsina dullness adeste corsage saraband promenaders gestational mansuetude fig redress pregame borshts pardoner reforges refutations calendal moaning doggerel dendrology governs ribonucleic circumscriptions reassimilating machinize rebuilding mezcal fluoresced antepenults blacksmith constance furores chroniclers overlie hoers jabbing resigner quartics polishers mallow hovelling ch',
-    'biorhythmic personalizing abjure greets rewashed thruput kashmir chores fiendishly combatting alliums lolly milder postpaid larry annuli codgers apostatizing scrim carillon rust grimly lignifying lycanthrope samisen founds millimeters pentagon humidification checkup hilts agonise crumbs rejected kangaroo forenoons grazable acidy duellist potent recyclability capture memorized psalmed meters decline deduced after oversolicitousness demoralizers ologist conscript cronyisms melodized girdles nonago',
-    'hermitage rejoices oxgall bloodstone fisticuff huguenot janitress assailed eggcup jerseyites fetas leipzig copiers pushiness fesse precociously modules navigates gaiters caldrons lisp humbly datum recite haphazardly dispassion calculability circularization intangibles impressionist jaggy ascribable overseen copses devolvement permutationists potations linesmen hematic fowler pridefully inversive malthus remainders multiplex petty hymnaries cubby donne ohioans avenues reverts glide photos antiaci',
-    'leonardo oxygenate cascade fashion fortifiers annelids co intimates cads expanse rusting quashing julienne hydrothermal defunctive permeation sabines hurries precalculates discourteously fooling pestles pellucid circlers hampshirites punchiest extremist cottonwood dadoes identifiers retail gyrations dusked opportunities ictus misjudge neighborly aulder larges predestinate bandstand angling billet drawbridge pantomimes propelled leaned gerontologists candying ingestive museum chlorites maryland s',
-    'undercurrents laryngeal elevate betokened chronologist ghostwrites ombres dollying airship probates music debouching countermanded rivalling linky wheedled heydey sours nitrates bewares rideable woven rerecorded currie vasectomize mousings rootstocks langley propaganda numismatics foetor subduers babcock jauntily ascots nested notifying mountainside dirk chancellors disassociating eleganter radiant convexity appositeness axonic trainful nestlers applicably correctional stovers organdy bdrm insis'
-]
+class Cipher_Generator:
+    def __init__(self):
+        global possible_chars, num_possible_chars, test1_plaintexts
+        possible_chars='abcdefghijklmnopqrstuvwxyz '
+        num_possible_chars = 27
 
-random.seed()
+        test1_plaintexts = [
+            'cabooses meltdowns bigmouth makework flippest neutralizers gipped mule antithetical imperials carom masochism stair retsina dullness adeste corsage saraband promenaders gestational mansuetude fig redress pregame borshts pardoner reforges refutations calendal moaning doggerel dendrology governs ribonucleic circumscriptions reassimilating machinize rebuilding mezcal fluoresced antepenults blacksmith constance furores chroniclers overlie hoers jabbing resigner quartics polishers mallow hovelling ch',
+            'biorhythmic personalizing abjure greets rewashed thruput kashmir chores fiendishly combatting alliums lolly milder postpaid larry annuli codgers apostatizing scrim carillon rust grimly lignifying lycanthrope samisen founds millimeters pentagon humidification checkup hilts agonise crumbs rejected kangaroo forenoons grazable acidy duellist potent recyclability capture memorized psalmed meters decline deduced after oversolicitousness demoralizers ologist conscript cronyisms melodized girdles nonago',
+            'hermitage rejoices oxgall bloodstone fisticuff huguenot janitress assailed eggcup jerseyites fetas leipzig copiers pushiness fesse precociously modules navigates gaiters caldrons lisp humbly datum recite haphazardly dispassion calculability circularization intangibles impressionist jaggy ascribable overseen copses devolvement permutationists potations linesmen hematic fowler pridefully inversive malthus remainders multiplex petty hymnaries cubby donne ohioans avenues reverts glide photos antiaci',
+            'leonardo oxygenate cascade fashion fortifiers annelids co intimates cads expanse rusting quashing julienne hydrothermal defunctive permeation sabines hurries precalculates discourteously fooling pestles pellucid circlers hampshirites punchiest extremist cottonwood dadoes identifiers retail gyrations dusked opportunities ictus misjudge neighborly aulder larges predestinate bandstand angling billet drawbridge pantomimes propelled leaned gerontologists candying ingestive museum chlorites maryland s',
+            'undercurrents laryngeal elevate betokened chronologist ghostwrites ombres dollying airship probates music debouching countermanded rivalling linky wheedled heydey sours nitrates bewares rideable woven rerecorded currie vasectomize mousings rootstocks langley propaganda numismatics foetor subduers babcock jauntily ascots nested notifying mountainside dirk chancellors disassociating eleganter radiant convexity appositeness axonic trainful nestlers applicably correctional stovers organdy bdrm insis'
+        ]
 
-def schedule_key(i,t,l):
-    return i % t
+        random.seed()
 
-def shift(plaintext, key, t):
-    p_len = len(plaintext)
-    p_i = 0
-    c_i = 0
-    while p_i < p_len:
-        j = schedule_key(c_i,t,p_len)
-        if j > -1 and j < t:
-            p = ord(plaintext[p_i])
-            p = 26 if p == ord(' ') else p - ord('a')
-            c = possible_chars[(p + key[j]) % 27] 
-            p_i += 1
-        else:
-            c = random.choice(possible_chars)
-        yield c
-        c_i += 1
+    def schedule_key(self, i,t,l):
+        return i % t
 
-def encrypt(plaintext):
-    t = random.randint(1,24)
-    key = [random.randint(0,26) for i in range(t)]
-    print('Plaintext: ' + plaintext + '\n')
-    print('Key: [' + ','.join(str(k) for k in key) + ']\n')
-    print('Ciphertext: ' + ''.join(shift(plaintext, key, t)) + '\n')
+    def insert_random_chars(self, ciphertext, num_random = 50):
+        positions = [None for i in range(num_random)]
+        L = len(ciphertext)
 
-def generate_test1_cipher():
-    plain = random.choice(test1_plaintexts)
-    encrypt(plain)
+        for round in range(num_random):
+            num_added_so_far = len(positions)
+            pos = random.randint(L) + num_added_so_far
+            positions[ round ] = pos
+        
+        print('Random character locations', positions)
+
+        perturbed_ciphertext = []
+        positions_index = 0
+        num_added_so_far = 0
+        for i in range(L):
+            if num_added_so_far < num_random:
+                pos = positions[ num_added_so_far ]
+                if i + num_added_so_far == pos:
+                    random_char = random.choice(possible_chars)
+                    perturbed_ciphertext.append(random_char)
+                    num_added_so_far += 1
+            perturbed_ciphertext.append( ciphertext[i] )
+        
+        return ''.join(perturbed_ciphertext)
+            
+    def shift(self, plaintext, key, t):
+        p_len = len(plaintext)
+        p_i = 0
+        c_i = 0
+        while p_i < p_len:
+            j = self.schedule_key(c_i,t,p_len)
+            if j > -1 and j < t:
+                p = ord(plaintext[p_i])
+                p = 26 if p == ord(' ') else p - ord('a')
+                c = possible_chars[(p + key[j]) % 27] 
+                p_i += 1
+            else:
+                c = random.choice(possible_chars)
+            yield c
+            c_i += 1
+
+    def encrypt(self, plaintext, add_random_chars = False, num_random_chars = 50):
+        t = random.randint(1,24)
+        key = [random.randint(0,26) for i in range(t)]
+        ciphertext = ''.join( self.shift(plaintext, key, t))
+        print('Plaintext: ' + plaintext + '\n')
+        print('Key: [' + ','.join(str(k) for k in key) + ']\n')
+        print('Ciphertext: ' + ciphertext + '\n')
+        if add_random_chars:
+            ciphertext = self.insert_random_chars(ciphertext, num_random_chars)
+            print(f'Ciphertext with %d random chars', ciphertext)
+        return ciphertext
+
+    def generate_test1_cipher(plain_text_number=1, add_random_chars= False, num_random_chars = 50):
+        # plain = random.choice(test1_plaintexts)
+        plain = test1_plaintexts[ plain_text_number ]
+        return self.encrypt(plain, add_random_chars, num_random_chars)
     
 
-if __name__ == '__main__':
-    generate_test1_cipher()
+# if __name__ == '__main__':
+#     cipher_generator = Cipher_Generator()
+    
